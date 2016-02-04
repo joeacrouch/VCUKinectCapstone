2934320609dc732b2a7ab86c67865864a0ba19a0
joeacrouch <crouchja@vcu.edu>
2016-02-04 14:32:23 -0500

    added switch between IR and RGB, also skeleton tracking on 1 sensor

--- Kinect_360_sample/MainWindow.xaml.cs
+++ Kinect_360_sample/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Media;
 using System.Windows.Media.Animation;
 using Microsoft.Kinect;
 using Microsoft.Kinect.Toolkit;
@@ -20,7 +19,6 @@ using Microsoft.Runtime;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
-using System.Windows.Media.Imaging;
 namespace Kinect_360_sample
 {
 
@@ -47,11 +45,63 @@ namespace Kinect_360_sample
         private byte[] depthPixels;
         private byte[] colorPixels2;
         private byte[] depthPixels2;
+
+        private const float RenderWidth = 640.0f;
+        private const float RenderHeight = 480.0f;
+        private const double JointThickness = 3;
+        private const double BodyCenterThickness = 10;
+        private const double ClipBoundsThickness = 10;
+        private readonly Brush centerPointBrush = Brushes.Blue;
+        private readonly Brush trackedJointBrush = new SolidColorBrush(Color.FromArgb(255, 68, 192, 68));
+        private readonly Brush inferredJointBrush = Brushes.Yellow;
+        private readonly Pen trackedBonePen = new Pen(Brushes.Green, 6);
+        private readonly Pen inferredBonePen = new Pen(Brushes.Gray, 1);
+
+        private DrawingGroup drawingGroup;
+        private DrawingImage imageSource;
+        private DrawingGroup drawingGroup2;
+        private DrawingImage imageSource2;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        
+
+        private static void RenderClippedEdges(Skeleton skeleton, DrawingContext drawingContext)
+        {
+            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Bottom))
+            {
+                drawingContext.DrawRectangle(
+                    Brushes.Red,
+                    null,
+                    new Rect(0, RenderHeight - ClipBoundsThickness, RenderWidth, ClipBoundsThickness));
+            }
+
+            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Top))
+            {
+                drawingContext.DrawRectangle(
+                    Brushes.Red,
+                    null,
+                    new Rect(0, 0, RenderWidth, ClipBoundsThickness));
+            }
+
+            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Left))
+            {
+                drawingContext.DrawRectangle(
+                    Brushes.Red,
+                    null,
+                    new Rect(0, 0, ClipBoundsThickness, RenderHeight));
+            }
+
+            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Right))
+            {
+                drawingContext.DrawRectangle(
+                    Brushes.Red,
+                    null,
+                    new Rect(RenderWidth - ClipBoundsThickness, 0, ClipBoundsThickness, RenderHeight));
+            }
+        }
+
 
         private void Window_Loaded_1(object sender, RoutedEventArgs e)
         {
@@ -62,6 +112,24 @@ namespace Kinect_360_sample
             deviceText2.Text = "Device ID 2 : ";
             TextBlock deviceIDtext = new TextBlock();
             TextBlock deviceIDtext2 = new TextBlock();
+
+            // Create the drawing group we'll use for drawing
+            this.drawingGroup = new DrawingGroup();
+
+            // Create an image source that we can use in our image control
+            this.imageSource = new DrawingImage(this.drawingGroup);
+
+            // Display the drawing using our image control
+            skeletonimage.Source = this.imageSource;
+
+            // Create the drawing group we'll use for drawing
+            this.drawingGroup2 = new DrawingGroup();
+
+            // Create an image source that we can use in our image control
+            this.imageSource2 = new DrawingImage(this.drawingGroup2);
+
+            // Display the drawing using our image control
+            skeletonimage2.Source = this.imageSource2;
 
             foreach (var potentialSensor in KinectSensor.KinectSensors)
             {
@@ -114,6 +182,15 @@ namespace Kinect_360_sample
                 // Add an event handler to be called whenever there is new color frame data
                 this.sensor.ColorFrameReady += this.SensorColorFrameReady;
 
+    //skeleton
+                // Turn on the skeleton stream to receive skeleton frames
+                this.sensor.SkeletonStream.Enable();
+
+                // Add an event handler to be called whenever there is new color frame data
+                this.sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+
+   
+    //device text info
                 deviceIDtext.Text = sensor.UniqueKinectId;
                 stack1.Children.Add(deviceText);
                 stack1.Children.Add(deviceIDtext);
@@ -167,6 +244,15 @@ namespace Kinect_360_sample
 
                 // Add an event handler to be called whenever there is new color frame data
                 this.sensor2.ColorFrameReady += this.SensorColorFrameReady2;
+
+
+                //skeleton
+                // Turn on the skeleton stream to receive skeleton frames
+                this.sensor2.SkeletonStream.Enable();
+
+                // Add an event handler to be called whenever there is new color frame data
+                this.sensor2.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+
                 // Start the sensor!
                 try
                 {
@@ -185,6 +271,96 @@ namespace Kinect_360_sample
 
         }
 
+        private void SwitchRGBtoIR1(object sender, RoutedEventArgs e)
+        {
+            if (this.checkIR1Mode.IsChecked.GetValueOrDefault())
+            {
+                if (null != this.sensor)
+                {
+                    // Turn on the color stream to receive color frames
+                    this.sensor.ColorStream.Enable(ColorImageFormat.InfraredResolution640x480Fps30);
+
+                    // Allocate space to put the pixels we'll receive
+                    this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+
+                    // This is the bitmap we'll display on-screen
+                    this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Gray16, null);
+
+                    // Set the image we display to point to the bitmap where we'll put the image data
+                    this.colorimage.Source = this.colorBitmap;
+
+                    // Add an event handler to be called whenever there is new color frame data
+                    this.sensor.ColorFrameReady += this.SensorColorFrameReady;
+                }
+
+            }
+            else
+            {
+                if (null != this.sensor)
+                {
+                    // Turn on the color stream to receive color frames
+                    this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+                    // Allocate space to put the pixels we'll receive
+                    this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+
+                    // This is the bitmap we'll display on-screen
+                    this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                    // Set the image we display to point to the bitmap where we'll put the image data
+                    this.colorimage.Source = this.colorBitmap;
+
+                    // Add an event handler to be called whenever there is new color frame data
+                    this.sensor.ColorFrameReady += this.SensorColorFrameReady;
+                }
+            }
+        }
+
+        private void SwitchRGBtoIR2(object sender, RoutedEventArgs e)
+        {
+            if (this.checkIR2Mode.IsChecked.GetValueOrDefault())
+            {
+                if (null != this.sensor2)
+                {
+                    // Turn on the color stream to receive color frames
+                    this.sensor2.ColorStream.Enable(ColorImageFormat.InfraredResolution640x480Fps30);
+
+                    // Allocate space to put the pixels we'll receive
+                    this.colorPixels2 = new byte[this.sensor2.ColorStream.FramePixelDataLength];
+
+                    // This is the bitmap we'll display on-screen
+                    this.colorBitmap2 = new WriteableBitmap(this.sensor2.ColorStream.FrameWidth, this.sensor2.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Gray16, null);
+
+                    // Set the image we display to point to the bitmap where we'll put the image data
+                    this.colorimage2.Source = this.colorBitmap2;
+
+                    // Add an event handler to be called whenever there is new color frame data
+                    this.sensor2.ColorFrameReady += this.SensorColorFrameReady2;
+
+                }
+            }
+            else
+            {
+                if (null != this.sensor2)
+                {
+                    // Turn on the color stream to receive color frames
+                    this.sensor2.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+                    // Allocate space to put the pixels we'll receive
+                    this.colorPixels2 = new byte[this.sensor2.ColorStream.FramePixelDataLength];
+
+                    // This is the bitmap we'll display on-screen
+                    this.colorBitmap2 = new WriteableBitmap(this.sensor2.ColorStream.FrameWidth, this.sensor2.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                    // Set the image we display to point to the bitmap where we'll put the image data
+                    this.colorimage2.Source = this.colorBitmap2;
+
+                    // Add an event handler to be called whenever there is new color frame data
+                    this.sensor2.ColorFrameReady += this.SensorColorFrameReady2;
+                }
+            }
+        }
+
         private void SensorColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
         {
             using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
@@ -198,7 +374,7 @@ namespace Kinect_360_sample
                     this.colorBitmap.WritePixels(
                         new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
                         this.colorPixels,
-                        this.colorBitmap.PixelWidth * sizeof(int),
+                        this.colorBitmap.PixelWidth * colorFrame.BytesPerPixel,
                         0);
                 }
             }
@@ -210,13 +386,21 @@ namespace Kinect_360_sample
                 if (colorFrame != null)
                 {
                     // Copy the pixel data from the image to a temporary array
-                    colorFrame.CopyPixelDataTo(this.colorPixels2);
+                    try
+                    {
+                        colorFrame.CopyPixelDataTo(this.colorPixels2);
+                    }
+                    catch
+                    {
+                        System.Console.WriteLine("Failed");
+                        return;
+                    }
 
                     // Write the pixel data into our bitmap
                     this.colorBitmap2.WritePixels(
                         new Int32Rect(0, 0, this.colorBitmap2.PixelWidth, this.colorBitmap2.PixelHeight),
                         this.colorPixels2,
-                        this.colorBitmap2.PixelWidth * sizeof(int),
+                        this.colorBitmap2.PixelWidth * colorFrame.BytesPerPixel,
                         0);
                 }
             }
@@ -329,6 +513,180 @@ namespace Kinect_360_sample
                 }
             }
         }
+
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] skeletons = new Skeleton[0];
+
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+            }
+
+            using (DrawingContext dc = this.drawingGroup.Open())
+            {
+                // Draw a transparent background to set the render size
+                dc.DrawRectangle(Brushes.Black, null, new Rect(0.0, 0.0, RenderWidth, RenderHeight));
+
+                if (skeletons.Length != 0)
+                {
+                    foreach (Skeleton skel in skeletons)
+                    {
+                        RenderClippedEdges(skel, dc);
+
+                        if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            this.DrawBonesAndJoints(skel, dc);
+                        }
+                        else if (skel.TrackingState == SkeletonTrackingState.PositionOnly)
+                        {
+                            dc.DrawEllipse(
+                            this.centerPointBrush,
+                            null,
+                            this.SkeletonPointToScreen(skel.Position),
+                            BodyCenterThickness,
+                            BodyCenterThickness);
+                        }
+                    }
+                }
+
+                // prevent drawing outside of our render area
+                this.drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(0.0, 0.0, RenderWidth, RenderHeight));
+            }
+        }
+
+        /// <summary>
+        /// Draws a skeleton's bones and joints
+        /// </summary>
+        /// <param name="skeleton">skeleton to draw</param>
+        /// <param name="drawingContext">drawing context to draw to</param>
+        private void DrawBonesAndJoints(Skeleton skeleton, DrawingContext drawingContext)
+        {
+            // Render Torso
+            this.DrawBone(skeleton, drawingContext, JointType.Head, JointType.ShoulderCenter);
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderRight);
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.Spine);
+            this.DrawBone(skeleton, drawingContext, JointType.Spine, JointType.HipCenter);
+            this.DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipRight);
+
+            // Left Arm
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderLeft, JointType.ElbowLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.ElbowLeft, JointType.WristLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.WristLeft, JointType.HandLeft);
+
+            // Right Arm
+            this.DrawBone(skeleton, drawingContext, JointType.ShoulderRight, JointType.ElbowRight);
+            this.DrawBone(skeleton, drawingContext, JointType.ElbowRight, JointType.WristRight);
+            this.DrawBone(skeleton, drawingContext, JointType.WristRight, JointType.HandRight);
+
+            // Left Leg
+            this.DrawBone(skeleton, drawingContext, JointType.HipLeft, JointType.KneeLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.KneeLeft, JointType.AnkleLeft);
+            this.DrawBone(skeleton, drawingContext, JointType.AnkleLeft, JointType.FootLeft);
+
+            // Right Leg
+            this.DrawBone(skeleton, drawingContext, JointType.HipRight, JointType.KneeRight);
+            this.DrawBone(skeleton, drawingContext, JointType.KneeRight, JointType.AnkleRight);
+            this.DrawBone(skeleton, drawingContext, JointType.AnkleRight, JointType.FootRight);
+
+            // Render Joints
+            foreach (Joint joint in skeleton.Joints)
+            {
+                Brush drawBrush = null;
+
+                if (joint.TrackingState == JointTrackingState.Tracked)
+                {
+                    drawBrush = this.trackedJointBrush;
+                }
+                else if (joint.TrackingState == JointTrackingState.Inferred)
+                {
+                    drawBrush = this.inferredJointBrush;
+                }
+
+                if (drawBrush != null)
+                {
+                    drawingContext.DrawEllipse(drawBrush, null, this.SkeletonPointToScreen(joint.Position), JointThickness, JointThickness);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Maps a SkeletonPoint to lie within our render space and converts to Point
+        /// </summary>
+        /// <param name="skelpoint">point to map</param>
+        /// <returns>mapped point</returns>
+        private Point SkeletonPointToScreen(SkeletonPoint skelpoint)
+        {
+            // Convert point to depth space.  
+            // We are not using depth directly, but we do want the points in our 640x480 output resolution.
+            DepthImagePoint depthPoint = this.sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skelpoint, DepthImageFormat.Resolution640x480Fps30);
+            return new Point(depthPoint.X, depthPoint.Y);
+        }
+
+        /// <summary>
+        /// Draws a bone line between two joints
+        /// </summary>
+        /// <param name="skeleton">skeleton to draw bones from</param>
+        /// <param name="drawingContext">drawing context to draw to</param>
+        /// <param name="jointType0">joint to start drawing from</param>
+        /// <param name="jointType1">joint to end drawing at</param>
+        private void DrawBone(Skeleton skeleton, DrawingContext drawingContext, JointType jointType0, JointType jointType1)
+        {
+            Joint joint0 = skeleton.Joints[jointType0];
+            Joint joint1 = skeleton.Joints[jointType1];
+
+            // If we can't find either of these joints, exit
+            if (joint0.TrackingState == JointTrackingState.NotTracked ||
+                joint1.TrackingState == JointTrackingState.NotTracked)
+            {
+                return;
+            }
+
+            // Don't draw if both points are inferred
+            if (joint0.TrackingState == JointTrackingState.Inferred &&
+                joint1.TrackingState == JointTrackingState.Inferred)
+            {
+                return;
+            }
+
+            // We assume all drawn bones are inferred unless BOTH joints are tracked
+            Pen drawPen = this.inferredBonePen;
+            if (joint0.TrackingState == JointTrackingState.Tracked && joint1.TrackingState == JointTrackingState.Tracked)
+            {
+                drawPen = this.trackedBonePen;
+            }
+
+            drawingContext.DrawLine(drawPen, this.SkeletonPointToScreen(joint0.Position), this.SkeletonPointToScreen(joint1.Position));
+        }
+
+        /// <summary>
+        /// Handles the checking or unchecking of the seated mode combo box
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void CheckBoxSeatedModeChanged(object sender, RoutedEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                if (this.checkBoxSeatedMode.IsChecked.GetValueOrDefault())
+                {
+                    this.sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Seated;
+                }
+                else
+                {
+                    this.sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
+                }
+            }
+        }
+
+
+
         private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
         {
             if (null != this.sensor)
