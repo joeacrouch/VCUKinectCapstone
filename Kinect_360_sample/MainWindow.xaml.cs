87690aabfc4ef8c9e8be0db26667be1a92ae8e96
joeacrouch <crouchja@vcu.edu>
2016-02-04 10:53:36 -0500

    Kinect 2 kinects working code 2/4/16
    Working Code for 2 Kinects

---
+++ Kinect_360_sample/MainWindow.xaml.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using Microsoft.Kinect;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Runtime;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Windows.Media.Imaging;
+namespace Kinect_360_sample
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensorChooser sensorChooser1;
+        private KinectSensorChooser sensorChooser2;
+        private KinectSensorChooserUI sensorChooserUi1;
+        private KinectSensorChooserUI sensorChooserUi2;
+        private KinectSensor sensor;
+        private KinectSensor sensor2;
+        private WriteableBitmap depthBitmap;
+        private WriteableBitmap colorBitmap;
+        private WriteableBitmap depthBitmap2;
+        private WriteableBitmap colorBitmap2;
+        /// <summary>
+        /// Intermediate storage for the depth data received from the camera
+        /// </summary>
+        private DepthImagePixel[] depthImagePixels;
+        private DepthImagePixel[] depthImagePixels2;
+        /// <summary>
+        /// Intermediate storage for the depth data converted to color
+        /// </summary>
+        private byte[] colorPixels;
+        private byte[] depthPixels;
+        private byte[] colorPixels2;
+        private byte[] depthPixels2;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            bool connectedSensor = false;
+            TextBlock deviceText = new TextBlock();
+            deviceText.Text = "Device ID : ";
+            TextBlock deviceText2 = new TextBlock();
+            deviceText2.Text = "Device ID 2 : ";
+            TextBlock deviceIDtext = new TextBlock();
+            TextBlock deviceIDtext2 = new TextBlock();
+
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected && connectedSensor == false)
+                {
+                    this.sensor = potentialSensor;
+                    connectedSensor = true;
+                }
+                if (potentialSensor.Status == KinectStatus.Connected && potentialSensor != sensor) {
+                    sensor2 = potentialSensor;
+
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                // Turn on the depth stream to receive depth frames
+                this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+
+                // Allocate space to put the depth pixels we'll receive
+                this.depthImagePixels = new DepthImagePixel[this.sensor.DepthStream.FramePixelDataLength];
+
+                // Allocate space to put the color pixels we'll create
+                this.depthPixels = new byte[this.sensor.DepthStream.FramePixelDataLength * sizeof(int)];
+
+                // This is the bitmap we'll display on-screen
+                this.depthBitmap = new WriteableBitmap(this.sensor.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 65.0, 65.0, PixelFormats.Bgr32, null);
+
+                // Set the image we display to point to the bitmap where we'll put the image data
+                this.depthimage.Source = this.depthBitmap;
+
+                // Add an event handler to be called whenever there is new depth frame data
+                this.sensor.DepthFrameReady += this.SensorDepthFrameReady;
+
+
+
+                // Turn on the color stream to receive color frames
+                this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+                // Allocate space to put the pixels we'll receive
+                this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
+                
+                // This is the bitmap we'll display on-screen
+                this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+                
+                // Set the image we display to point to the bitmap where we'll put the image data
+                this.colorimage.Source = this.colorBitmap;
+
+                // Add an event handler to be called whenever there is new color frame data
+                this.sensor.ColorFrameReady += this.SensorColorFrameReady;
+
+                deviceIDtext.Text = sensor.UniqueKinectId;
+                stack1.Children.Add(deviceText);
+                stack1.Children.Add(deviceIDtext);
+
+                
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+            if (null != this.sensor2) {
+                deviceIDtext2.Text = sensor2.UniqueKinectId;
+                stack1.Children.Add(deviceText2);
+                stack1.Children.Add(deviceIDtext2);
+                // Turn on the depth stream to receive depth frames
+                this.sensor2.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+
+                // Allocate space to put the depth pixels we'll receive
+                this.depthImagePixels2 = new DepthImagePixel[this.sensor2.DepthStream.FramePixelDataLength];
+
+                // Allocate space to put the color pixels we'll create
+                this.depthPixels2 = new byte[this.sensor2.DepthStream.FramePixelDataLength * sizeof(int)];
+
+                // This is the bitmap we'll display on-screen
+                this.depthBitmap2 = new WriteableBitmap(this.sensor2.DepthStream.FrameWidth, this.sensor.DepthStream.FrameHeight, 65.0, 65.0, PixelFormats.Bgr32, null);
+
+                // Set the image we display to point to the bitmap where we'll put the image data
+                this.depthimage2.Source = this.depthBitmap2;
+
+                // Add an event handler to be called whenever there is new depth frame data
+                this.sensor2.DepthFrameReady += this.SensorDepthFrameReady2;
+
+
+
+                // Turn on the color stream to receive color frames
+                this.sensor2.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
+
+                // Allocate space to put the pixels we'll receive
+                this.colorPixels2 = new byte[this.sensor2.ColorStream.FramePixelDataLength];
+
+                // This is the bitmap we'll display on-screen
+                this.colorBitmap2 = new WriteableBitmap(this.sensor2.ColorStream.FrameWidth, this.sensor2.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+                // Set the image we display to point to the bitmap where we'll put the image data
+                this.colorimage2.Source = this.colorBitmap2;
+
+                // Add an event handler to be called whenever there is new color frame data
+                this.sensor2.ColorFrameReady += this.SensorColorFrameReady2;
+                // Start the sensor!
+                try
+                {
+                    this.sensor2.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor2 = null;
+                }
+            }
+            if (null == this.sensor)
+            {
+               // this.statusBarText.Text = Properties.Resources.NoKinectReady;
+            }
+
+        }
+
+        private void SensorColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame != null)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    colorFrame.CopyPixelDataTo(this.colorPixels);
+
+                    // Write the pixel data into our bitmap
+                    this.colorBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight),
+                        this.colorPixels,
+                        this.colorBitmap.PixelWidth * sizeof(int),
+                        0);
+                }
+            }
+        }
+        private void SensorColorFrameReady2(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+            {
+                if (colorFrame != null)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    colorFrame.CopyPixelDataTo(this.colorPixels2);
+
+                    // Write the pixel data into our bitmap
+                    this.colorBitmap2.WritePixels(
+                        new Int32Rect(0, 0, this.colorBitmap2.PixelWidth, this.colorBitmap2.PixelHeight),
+                        this.colorPixels2,
+                        this.colorBitmap2.PixelWidth * sizeof(int),
+                        0);
+                }
+            }
+        }
+        private void SensorDepthFrameReady(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame != null)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    depthFrame.CopyDepthImagePixelDataTo(this.depthImagePixels);
+
+                    // Get the min and max reliable depth for the current frame
+                    int minDepth = depthFrame.MinDepth;
+                    int maxDepth = depthFrame.MaxDepth;
+
+                    // Convert the depth to RGB
+                    int colorPixelIndex = 0;
+                    for (int i = 0; i < this.depthImagePixels.Length; ++i)
+                    {
+                        // Get the depth for this pixel
+                        short depth = depthImagePixels[i].Depth;
+
+                        // To convert to a byte, we're discarding the most-significant
+                        // rather than least-significant bits.
+                        // We're preserving detail, although the intensity will "wrap."
+                        // Values outside the reliable depth range are mapped to 0 (black).
+
+                        // Note: Using conditionals in this loop could degrade performance.
+                        // Consider using a lookup table instead when writing production code.
+                        // See the KinectDepthViewer class used by the KinectExplorer sample
+                        // for a lookup table example.
+                        byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                        // Write out blue byte
+                        this.depthPixels[colorPixelIndex++] = intensity;
+
+                        // Write out green byte
+                        this.depthPixels[colorPixelIndex++] = intensity;
+
+                        // Write out red byte                        
+                        this.depthPixels[colorPixelIndex++] = intensity;
+
+                        // We're outputting BGR, the last byte in the 32 bits is unused so skip it
+                        // If we were outputting BGRA, we would write alpha here.
+                        ++colorPixelIndex;
+                    }
+
+                    // Write the pixel data into our bitmap
+                    this.depthBitmap.WritePixels(
+                        new Int32Rect(0, 0, this.depthBitmap.PixelWidth, this.depthBitmap.PixelHeight),
+                        this.depthPixels,
+                        this.depthBitmap.PixelWidth * sizeof(int),
+                        0);
+                }
+            }
+        }
+        private void SensorDepthFrameReady2(object sender, DepthImageFrameReadyEventArgs e)
+        {
+            using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+            {
+                if (depthFrame != null)
+                {
+                    // Copy the pixel data from the image to a temporary array
+                    depthFrame.CopyDepthImagePixelDataTo(this.depthImagePixels2);
+
+                    // Get the min and max reliable depth for the current frame
+                    int minDepth = depthFrame.MinDepth;
+                    int maxDepth = depthFrame.MaxDepth;
+
+                    // Convert the depth to RGB
+                    int colorPixelIndex = 0;
+                    for (int i = 0; i < this.depthImagePixels2.Length; ++i)
+                    {
+                        // Get the depth for this pixel
+                        short depth = depthImagePixels2[i].Depth;
+
+                        // To convert to a byte, we're discarding the most-significant
+                        // rather than least-significant bits.
+                        // We're preserving detail, although the intensity will "wrap."
+                        // Values outside the reliable depth range are mapped to 0 (black).
+
+                        // Note: Using conditionals in this loop could degrade performance.
+                        // Consider using a lookup table instead when writing production code.
+                        // See the KinectDepthViewer class used by the KinectExplorer sample
+                        // for a lookup table example.
+                        byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                        // Write out blue byte
+                        this.depthPixels2[colorPixelIndex++] = intensity;
+
+                        // Write out green byte
+                        this.depthPixels2[colorPixelIndex++] = intensity;
+
+                        // Write out red byte                        
+                        this.depthPixels2[colorPixelIndex++] = intensity;
+
+                        // We're outputting BGR, the last byte in the 32 bits is unused so skip it
+                        // If we were outputting BGRA, we would write alpha here.
+                        ++colorPixelIndex;
+                    }
+
+                    // Write the pixel data into our bitmap
+                    this.depthBitmap2.WritePixels(
+                        new Int32Rect(0, 0, this.depthBitmap2.PixelWidth, this.depthBitmap2.PixelHeight),
+                        this.depthPixels2,
+                        this.depthBitmap2.PixelWidth * sizeof(int),
+                        0);
+                }
+            }
+        }
+        private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }
+            if (null != this.sensor2)
+            {
+                this.sensor2.Stop();
+            }
+
+        }
+    }
+}
